c75f796be27b501ea77f63401eccfc714a4d7532
Samyusuf101 <samyusuf101@gmail.com>
2022-12-21 01:33:28 -0800

    created task folder

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,6 +11,14 @@ async function main() {
     await simpleStorage.deployTransaction.wait(6);
     await verify(simpleStorage.address, []);
   }
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`current value is : ${currentValue}`);
+
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`updated value is : ${updatedValue}`);
 }
 
 async function verify(contractAddress, args) {
