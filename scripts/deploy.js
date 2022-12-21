042408b4113b60058eccaae75544ddf9b7d4d238
Samyusuf101 <samyusuf101@gmail.com>
2022-12-21 01:01:16 -0800

    created the verify function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
 
 async function main() {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
@@ -7,6 +7,26 @@ async function main() {
   simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract to ${simpleStorage.address}`);
+  if (network.config.chainId === 4 && process.env.ETH_PRIVATE_KEY) {
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("verifying contracts");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("already verified");
+    } else {
+      console.log(e);
+    }
+  }
 }
 
 main()
